0d9095305b2f69bb207aa972e099d6827f470fcb
antigones <antigones@users.noreply.github.com>
2019-04-23 18:34:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+/*  register service worker */
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/sw.js')
+        .then(function() { console.log("Service Worker Registered"); });
+}
+
+fetch('content.json')
+	.then(function(response){
+		return response.json();
+	})
+	.then(function(data){
+        document.getElementById("the-title").innerHTML = data[0].title;
+		document.getElementById("the-content").innerHTML = data[0].text;
+	})
+	.catch(function(error){
+		alert('error : '+ error);
+	})
+
+
+
---
+++ sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'my-pwa-cache';
+var filesToCache = [
+	'/',
+	'/index.html',
+	'/css/style.css',
+	'/js/main.js',
+	'/images/cherry.png'
+];
+
+/* Start the service worker and cache app content */
+self.addEventListener('install', function(e) {
+	self.skipWaiting();
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+	e.respondWith(
+		caches.match(e.request).then(function(response) {
+			return response || fetch(e.request);
+		})
+	);
+});
